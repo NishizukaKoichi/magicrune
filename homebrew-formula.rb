8a109ff6186d6c94f70f904b06326b616b177908
GitHub Action <action@github.com>
2025-08-03 06:53:22 +0000

    Update Homebrew formula to v0.2.2

--- homebrew-formula.rb
+++ homebrew-formula.rb
@@ -1,14 +1,14 @@
 class Magicrune < Formula
   desc "Secure code execution framework with automatic sandbox detection"
   homepage "https://github.com/magicrune/magicrune"
-  version "0.2.1"
+  version "0.2.2"
   license "MIT"
 
   if Hardware::CPU.intel?
-    url "https://github.com/magicrune/magicrune/releases/download/v0.2.1/magicrune-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/magicrune/magicrune/releases/download/v0.2.2/magicrune-x86_64-apple-darwin.tar.gz"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   elsif Hardware::CPU.arm?
-    url "https://github.com/magicrune/magicrune/releases/download/v0.2.1/magicrune-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/magicrune/magicrune/releases/download/v0.2.2/magicrune-aarch64-apple-darwin.tar.gz"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
